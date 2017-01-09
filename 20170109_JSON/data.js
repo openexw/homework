9c4a94b6b55b0e552ccbe61651d7a45497d8881a
zhou1996 <1214780675@qq.com>
2017-01-09 20:57:38 +0800

    2017-1-9-9 JSON

---
+++ 20170109_JSON/data.js
@@ -0,0 +1,234 @@
+var data = {
+    "error": false,
+    "results": [
+    {
+        "_id": "56cc6d1d421aa95caa707540",
+        "createdAt": "2015-10-21T02:34:20.351Z",
+        "desc": "\u76ae\u7b4b\u5f0f\u5f39\u6027\u4e0b\u62c9\u5373\u5237\u65b0\u7ec4\u4ef6\uff0c\u6548\u679c\u5f88\u8d5e",
+        "publishedAt": "2015-10-22T02:06:07.743Z",
+        "type": "Android",
+        "url": "https://github.com/gontovnik/DGElasticPullToRefresh",
+        "used": true,
+        "who": "Dear\u5b85\u5b66\u957f"
+    },
+    {
+        "_id": "56cc6d29421aa95caa708166",
+        "createdAt": "2016-01-14T03:22:15.218Z",
+        "desc": "\u8ba9\u4f60\u7684\u5bc6\u7801\u52a8\u8d77\u6765",
+        "publishedAt": "2016-01-14T05:39:45.162Z",
+        "type": "Android",
+        "url": "https://github.com/Rogero0o/PasswordLoadingView",
+        "used": true,
+        "who": "Dear\u5b85\u5b66\u957f"
+    },
+    {
+        "_id": "5722c77567765974f5e27e53",
+        "createdAt": "2016-04-29T10:31:17.87Z",
+        "desc": "Android\u4e2dEnum\u5206\u6790\u4f7f\u7528",
+        "publishedAt": "2016-04-29T11:36:42.906Z",
+        "source": "chrome",
+        "type": "Android",
+        "url": "http://www.jianshu.com/p/6052cd4ea9ae",
+        "used": true,
+        "who": "AndWang"
+    },
+    {
+        "_id": "56cc6d1d421aa95caa7077d1",
+        "createdAt": "2015-08-22T03:28:52.221Z",
+        "desc": "\u56fe\u50cf\u88c1\u526a\u5e93",
+        "publishedAt": "2015-08-31T04:05:40.153Z",
+        "type": "Android",
+        "url": "https://github.com/IsseiAoki/SimpleCropView",
+        "used": true,
+        "who": "Jason"
+    },
+    {
+        "_id": "572765bf67765974f5e27e71",
+        "createdAt": "2016-05-02T22:35:43.321Z",
+        "desc": "\u901a\u8fc7\u5bf9\u6d41\u884c\u7684\u4f18\u8d28android\u5f00\u6e90\u9879\u76ee\u5206\u6790\u3001\u5b66\u4e60\u3001\u4eff\u5199\uff0c\u6700\u7ec8\u7406\u89e3\uff0c\u4e0d\u4ec5\u5e2e\u4f60\u638c\u63e1\u6700\u65b0\u7684android\u6280\u672f\uff0c\u66f4\u80fd\u8ba9\u4f60\u7406\u89e3\u5f00\u6e90\u9879\u76ee\u7684\u7cbe\u534e\u4e4b\u5904\u3002",
+        "publishedAt": "2016-05-03T12:13:53.904Z",
+        "source": "chrome",
+        "type": "Android",
+        "url": "https://github.com/wingjay/android-open-source-project-cracking",
+        "used": true,
+        "who": "Jason"
+    },
+    {
+        "_id": "570320ed6776596340b1d1ed",
+        "createdAt": "2016-04-05T10:20:29.315Z",
+        "desc": "\u5c01\u88c5\u90a3\u4e9b\u4e8b-RecyclerView\u5c01\u88c5\u5b9e\u8df5",
+        "publishedAt": "2016-04-05T10:45:46.487Z",
+        "source": "chrome",
+        "type": "Android",
+        "url": "http://www.jianshu.com/p/a6f158d1a9c9",
+        "used": true,
+        "who": "AndWang"
+    },
+    {
+        "_id": "56cc6d1d421aa95caa7077de",
+        "createdAt": "2015-08-24T02:27:40.869Z",
+        "desc": "\u4e00\u4e9bandroid\u5b89\u5168\u76f8\u5173\u6536\u96c6",
+        "publishedAt": "2015-08-24T04:06:07.600Z",
+        "type": "Android",
+        "url": "https://github.com/ashishb/android-security-awesome",
+        "used": true,
+        "who": "andyiac"
+    },
+    {
+        "_id": "56cc6d26421aa95caa707fed",
+        "createdAt": "2015-12-15T02:41:00.558Z",
+        "desc": "\u6c34\u6f2b\u6548\u679c\u7684 loading \u52a8\u753b",
+        "publishedAt": "2015-12-15T05:01:30.531Z",
+        "type": "Android",
+        "url": "https://github.com/lopspower/CircularFillableLoaders",
+        "used": true,
+        "who": "mthli"
+    },
+    {
+        "_id": "57a02814421aa90d36e960ea",
+        "createdAt": "2016-08-02T12:56:52.911Z",
+        "desc": "\u8fd8\u4e0d\u6765\u5b66\u70b9Lambda\u4e48?",
+        "publishedAt": "2016-08-03T11:12:47.159Z",
+        "source": "chrome",
+        "type": "Android",
+        "url": "http://blog.csdn.net/dd864140130/article/details/50603420",
+        "used": true,
+        "who": "sbbic"
+    },
+    {
+        "_id": "5861d08d421aa97237bca911",
+        "createdAt": "2016-12-27T10:23:09.303Z",
+        "desc": "\u975e\u5e38\u6f02\u4eae\u7684\u4e00\u6b3e Android Movie \u5de5\u5177\u3002",
+        "images": [
+            "http://img.gank.io/174cf3a7-bbd8-43aa-9f70-186774291815",
+            "http://img.gank.io/dee00011-aea9-49b1-b9c4-d97b18a89074",
+            "http://img.gank.io/5355efee-d276-46e4-b380-8415325ba749"
+        ],
+        "publishedAt": "2016-12-27T12:06:15.638Z",
+        "source": "chrome",
+        "type": "Android",
+        "url": "https://github.com/lawloretienne/MovieHub",
+        "used": true,
+        "who": "\u4ee3\u7801\u5bb6"
+    },
+    {
+        "_id": "56f104bc67765933d8be9119",
+        "createdAt": "2016-03-22T16:39:24.788Z",
+        "desc": "Android\u5b9e\u73b0\u591a\u6b21\u95ea\u9000\u540e\u6e05\u9664\u6570\u636e",
+        "publishedAt": "2016-03-23T10:59:23.106Z",
+        "source": "web",
+        "type": "Android",
+        "url": "http://sixwolf.net/blog/2016/03/22/Android%E5%AE%9E%E7%8E%B0%E5%A4%9A%E6%AC%A1%E9%97%AA%E9%80%80%E6%B8%85%E9%99%A4%E6%95%B0%E6%8D%AE/",
+        "used": true,
+        "who": "Tang Likang"
+    },
+    {
+        "_id": "57f86c57421aa95dd78e8db3",
+        "createdAt": "2016-10-08T11:47:35.802Z",
+        "desc": "\u4e00\u4e2a\u7b80\u6d01\u7684\u6c47\u7387\u8f6c\u6362\u8f6f\u4ef6\uff0c\u4f7f\u7528 kotlin \u7f16\u5199\uff0c\u901a\u8fc7 anko \u6846\u67b6\u7f16\u5199 UI \u5e76\u5b9e\u73b0\u6f02\u4eae\u7684\u4e3b\u9898\u52a8\u6001\u5207\u6362\u6548\u679c",
+        "images": [
+            "http://img.gank.io/35ae570a-f172-45f3-bb53-867d1292b476"
+        ],
+        "publishedAt": "2016-10-09T11:45:38.236Z",
+        "source": "web",
+        "type": "Android",
+        "url": "https://github.com/yiyuanliu/Currency",
+        "used": true,
+        "who": "Yiyuan Liu"
+    },
+    {
+        "_id": "56d5575e421aa906d367a50f",
+        "createdAt": "2016-03-01T16:48:30.668Z",
+        "desc": "\u7ea2\u5305\u7167\u7247\u5ba2\u6237\u7aef\u5f00\u53d1\u90a3\u4e9b\u4e8b\n",
+        "publishedAt": "2016-03-04T12:44:51.926Z",
+        "source": "chrome",
+        "type": "Android",
+        "url": "https://mp.weixin.qq.com/s?__biz=MzAwNDY1ODY2OQ==&mid=401327503&idx=1&sn=1d9fac7b648c3494d312da962f53bfbd&scene=1&srcid=0301k7pxY5WpJ5aUMzYuzbsG&key=710a5d99946419d94900b56402795d2be724c19afa3e2fa91bfa3567a8d98026a5d70fd9b5c09f351fe1d91fbd20ac3d&ascene=0&uin=OTA2MzUzODEz&devicetype=iMac+MacBookPro11%2C4+OSX+OSX+10.11.3+build(15D21)&version=11020201&pass_ticket=5jVPXBARhNFxI3y%2Fn2Q%2BCdJh1%2Fud9kdEMvN%2Bkjr%2B1mew4rd8tyB%2FaH5mFk%2FHMWDc",
+        "used": true,
+        "who": "AndWang\u7389\u9e4f"
+    },
+    {
+        "_id": "56cc6d26421aa95caa707eb6",
+        "createdAt": "2015-11-23T01:54:48.554Z",
+        "desc": "\u4e00\u4e2a\u7b80\u5316\u4e86\u8bf7\u6c42\u7684\u6743\u9650\u5728\u8fd0\u884c\u7684\u8fc7\u7a0b\u7684\u5e93\uff08\u80fd\u8ba9\u4f60\u66f4\u65b9\u4fbf\u5730\u5f00\u542f\u80a5\u7682\u5bb6\u4e22\u80a5\u7682\u6743\u9650\u54e6~\uff09",
+        "publishedAt": "2015-11-23T03:47:31.390Z",
+        "type": "Android",
+        "url": "https://github.com/karumi/Dexter",
+        "used": true,
+        "who": "\u6709\u65f6\u653e\u7eb5"
+    },
+    {
+        "_id": "576fb253421aa94f45912ea2",
+        "createdAt": "2016-06-26T18:45:39.187Z",
+        "desc": "\u4e00\u4e2a\u652f\u6301\u56db\u4e2a\u65b9\u5411\u5faa\u73af\u6eda\u52a8\u7684\u81ea\u5b9a\u4e49\u63a7\u4ef6",
+        "images": [
+            "https://github.com/oubowu/MarqueeLayoutLibrary/raw/master/pic/demo.gif"
+        ],
+        "publishedAt": "2016-07-20T16:09:07.721Z",
+        "source": "chrome",
+        "type": "Android",
+        "url": "https://github.com/oubowu/MarqueeLayoutLibrary",
+        "used": true,
+        "who": "\u5927\u718a"
+    },
+    {
+        "_id": "56cc6d22421aa95caa7078f4",
+        "createdAt": "2015-10-03T03:42:28.848Z",
+        "desc": "Material Design Music Player",
+        "publishedAt": "2015-10-10T03:58:17.759Z",
+        "type": "Android",
+        "url": "https://github.com/naman14/Timber",
+        "used": true,
+        "who": "mthli"
+    },
+    {
+        "_id": "58232df6421aa9137697466f",
+        "createdAt": "2016-11-09T22:08:54.356Z",
+        "desc": "\u4e00\u4e2a\u652f\u6301\u9009\u8bcd\u7684 TextView\uff0c\u7c7b\u4f3c\u4e0e\u5355\u8bcd app \u4e2d\u70b9\u51fb\u5355\u8bcd\u7ffb\u8bd1\u7684\u6548\u679c",
+        "images": [
+            "http://img.gank.io/cfc82bd9-731f-4ac9-80db-67254f15baa1"
+        ],
+        "publishedAt": "2016-11-10T11:40:42.4Z",
+        "source": "web",
+        "type": "Android",
+        "url": "https://github.com/burgessjp/GetWordTextView",
+        "used": true,
+        "who": "solid"
+    },
+    {
+        "_id": "58232df6421aa9137697466f",
+        "createdAt": "2016-11-09T22:08:54.356Z",
+        "desc": "\u4e00\u4e2a\u652f\u6301\u9009\u8bcd\u7684 TextView\uff0c\u7c7b\u4f3c\u4e0e\u5355\u8bcd app \u4e2d\u70b9\u51fb\u5355\u8bcd\u7ffb\u8bd1\u7684\u6548\u679c",
+        "images": [
+            "http://img.gank.io/cfc82bd9-731f-4ac9-80db-67254f15baa1"
+        ],
+        "publishedAt": "2016-11-10T11:40:42.4Z",
+        "source": "web",
+        "type": "Android",
+        "url": "https://github.com/burgessjp/GetWordTextView",
+        "used": true,
+        "who": "solid"
+    },
+    {
+        "_id": "5721684a67765974f885bfbb",
+        "createdAt": "2016-04-28T09:32:58.363Z",
+        "desc": "Android drawable\u5fae\u6280\u5de7\uff0c\u4f60\u6240\u4e0d\u77e5\u9053\u7684drawable\u7684\u90a3\u4e9b\u7ec6\u8282",
+        "publishedAt": "2016-04-28T13:07:51.7Z",
+        "source": "chrome",
+        "type": "Android",
+        "url": "http://blog.csdn.net/guolin_blog/article/details/50727753#java",
+        "used": true,
+        "who": "\u848b\u670b"
+    },
+    {
+        "_id": "56cc6d29421aa95caa7080fb",
+        "createdAt": "2016-01-06T02:42:26.160Z",
+        "desc": "\u8bbe\u7f6erecycleView\u7684divider",
+        "publishedAt": "2016-01-06T05:04:37.18Z",
+        "type": "Android",
+        "url": "https://github.com/yqritc/RecyclerView-FlexibleDivider",
+        "used": true,
+        "who": "\u6709\u65f6\u653e\u7eb5"
+    }
+]
+};
