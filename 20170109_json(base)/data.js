b5586db9eb4897ee5d4315b7820a4e79f235c7f9
githubdeyao <1136471076@qq.com>
2017-01-09 20:33:39 +0800

    new

---
+++ 20170109_json(base)/data.js
@@ -0,0 +1,221 @@
+var data = {
+    "error": false,
+    "results": [
+        {
+            "_id": "56cc6d29421aa95caa7082b6",
+            "createdAt": "2016-02-16T03:19:57.547Z",
+            "desc": "\u4e00\u7cfb\u5217\u6709\u7528\u7684android\u7f51\u7edc\u5de5\u5177",
+            "publishedAt": "2016-02-16T04:46:45.166Z",
+            "type": "Android",
+            "url": "https://github.com/stealthcopter/AndroidNetworkTools",
+            "used": true,
+            "who": "MVP"
+        },
+        {
+            "_id": "56cc6d29421aa95caa70826c",
+            "createdAt": "2016-02-03T06:09:50.919Z",
+            "desc": "CodeBoy\u5fae\u4fe1\u62a2\u7ea2\u5305\u5916\u6302",
+            "publishedAt": "2016-02-04T07:14:01.792Z",
+            "type": "Android",
+            "url": "http://www.happycodeboy.com/index.php/archives/10/",
+            "used": true,
+            "who": "wuzheng"
+        },
+        {
+            "_id": "56e170f6677659666747aa73",
+            "createdAt": "2016-03-10T21:04:54.851Z",
+            "desc": "\u5229\u7528Window\u548cWindowManager\u5b9e\u73b0\u60ac\u6d6e\u7a97\u6548\u679c",
+            "publishedAt": "2016-03-18T12:18:39.928Z",
+            "source": "chrome",
+            "type": "Android",
+            "url": "http://blog.csdn.net/l664675249/article/details/50850051",
+            "used": true,
+            "who": "SparkYuan"
+        },
+        {
+            "_id": "56cc6d23421aa95caa70796d",
+            "createdAt": "2015-11-05T08:03:16.44Z",
+            "desc": "Buck: \u4e13\u4e3aAndroid\u6253\u9020\u7684\u6784\u5efa\u5de5\u5177(\u5c24\u5176\u9002\u5408\u5927\u578b\u5de5\u7a0b)",
+            "publishedAt": "2015-11-06T04:11:25.962Z",
+            "type": "Android",
+            "url": "http://zserge.com/blog/buck-build-system.html",
+            "used": true,
+            "who": "\u72c4\u5bb6\u6021"
+        },
+        {
+            "_id": "56f343dc67765933dbbd20ce",
+            "createdAt": "2016-03-24T09:33:16.34Z",
+            "desc": "\u952e\u76d8\u72b6\u6001\u76d1\u63a7",
+            "publishedAt": "2016-03-24T12:21:54.835Z",
+            "source": "chrome",
+            "type": "Android",
+            "url": "https://github.com/AzimoLabs/AndroidKeyboardWatcher",
+            "used": true,
+            "who": "\u82b1\u5f00\u582a\u6298\u679d"
+        },
+        {
+            "_id": "56cc6d1d421aa95caa7078c8",
+            "createdAt": "2015-09-21T02:22:15.343Z",
+            "desc": "\u4e00\u4e2aAndroid\u7684\u6ed1\u9f7f\u6548\u679c",
+            "publishedAt": "2015-09-21T03:38:47.261Z",
+            "type": "Android",
+            "url": "https://github.com/channguyen/range-slider-view",
+            "used": true,
+            "who": "\u6709\u65f6\u653e\u7eb5"
+        },
+        {
+            "_id": "571ed93567765974f5e27e14",
+            "createdAt": "2016-04-26T10:57:57.79Z",
+            "desc": "Android App\u7684\u8bbe\u8ba1\u67b6\u6784\uff1aMVC,MVP,MVVM\u4e0e\u67b6\u6784\u7ecf\u9a8c\u8c08",
+            "publishedAt": "2016-04-26T11:58:55.460Z",
+            "source": "web",
+            "type": "Android",
+            "url": "https://www.sdk.cn/news/2501",
+            "used": true,
+            "who": null
+        },
+        {
+            "_id": "56cc6d26421aa95caa707d03",
+            "createdAt": "2015-09-14T02:10:45.920Z",
+            "desc": "\u4e00\u4e2a\u7b80\u5355\u7684\u5de5\u5177\uff0c\u4ee5\u65b9\u4fbfbug\u62a5\u544a\u4f60\u7684\u5e94\u7528\u7a0b\u5e8f\u4e2d\u6355\u83b7\u3002",
+            "publishedAt": "2015-09-14T03:55:03.717Z",
+            "type": "Android",
+            "url": "https://github.com/mattprecious/telescope",
+            "used": true,
+            "who": "\u6709\u65f6\u653e\u7eb5"
+        },
+        {
+            "_id": "56cc6d1d421aa95caa70756d",
+            "createdAt": "2015-10-27T16:35:57.28Z",
+            "desc": "\u57fa\u7840\uff1agrep \u641c\u7d22\u76ee\u5f55\u4e2d\u7684\u6587\u4ef6",
+            "publishedAt": "2015-10-28T03:51:19.328Z",
+            "type": "Android",
+            "url": "http://blog.csdn.net/tsxw24/article/details/7828357",
+            "used": true,
+            "who": "LHF"
+        },
+        {
+            "_id": "56cc6d1d421aa95caa7076ce",
+            "createdAt": "2015-06-06T13:14:01.487Z",
+            "desc": "Google\u5b98\u65b9\u7684Chromium WebView\u4f7f\u7528\u793a\u4f8b",
+            "publishedAt": "2016-03-29T11:56:01.215Z",
+            "type": "Android",
+            "url": "https://github.com/GoogleChrome/chromium-webview-samples",
+            "used": true,
+            "who": "mthli"
+        },
+        {
+            "_id": "56cc6d1d421aa95caa707575",
+            "createdAt": "2015-10-27T11:44:01.853Z",
+            "desc": "\u7f8e\u56e2Android\u8d44\u6e90\u6df7\u6dc6\u4fdd\u62a4\u5b9e\u8df5",
+            "publishedAt": "2015-10-28T03:51:19.326Z",
+            "type": "Android",
+            "url": "http://android.jobbole.com/81915/",
+            "used": true,
+            "who": "wuzheng"
+        },
+        {
+            "_id": "5853c87c421aa9723a5a7776",
+            "createdAt": "2016-12-16T18:57:00.175Z",
+            "desc": "Android Things \u6765\u4e86\uff0c\u8001\u53f8\u673a\u5feb\u4e0a\u8f66~",
+            "images": [
+                "http://img.gank.io/60498fde-c356-45a7-8903-a678c6ebaa11"
+            ],
+            "publishedAt": "2016-12-27T12:06:15.638Z",
+            "source": "web",
+            "type": "Android",
+            "url": "https://makerdiary.co/getting-started-with-android-things/",
+            "used": true,
+            "who": "YottaStone"
+        },
+        {
+            "_id": "572aae1067765974f885c009",
+            "createdAt": "2016-05-05T10:21:04.34Z",
+            "desc": "Android\u8d85\u9ad8\u4effQQ\u9644\u8fd1\u7684\u4eba\u641c\u7d22\u5c55\u793a",
+            "publishedAt": "2016-05-05T12:14:21.156Z",
+            "source": "web",
+            "type": "Android",
+            "url": "https://github.com/ImmortalZ/RadarScan",
+            "used": true,
+            "who": "Mr_immortalZ"
+        },
+        {
+            "_id": "57a14895421aa90d36e960f7",
+            "createdAt": "2016-08-03T09:27:49.273Z",
+            "desc": "\u633a\u6709\u610f\u601d\u7684\u4e00\u4e2a\u4e0b\u62c9\u5237\u65b0\u6548\u679c\uff0c\u53ef\u4ee5\u770b\u770b\u6e90\u7801\u5b66\u4e60\u4e00\u4e0b\u5b9e\u73b0\u7c7b\u4f3c\u6548\u679c\u7684\u601d\u8def\u3002",
+            "images": [
+                "https://camo.githubusercontent.com/f390437ba548be66a9e4c91389e5587000533c24/68747470733a2f2f7261772e6769746875622e636f6d2f6c7562656173742f50756c6c4c61756e6368526f636b65742f6d61737465722f73637265656e73686f74732f726f636b65742e676966"
+            ],
+            "publishedAt": "2016-08-03T11:12:47.159Z",
+            "source": "chrome",
+            "type": "Android",
+            "url": "https://github.com/lubeast/PullToRefresh",
+            "used": true,
+            "who": "\u4ee3\u7801\u5bb6"
+        },
+        {
+            "_id": "56cc6d26421aa95caa707eaf",
+            "createdAt": "2015-11-21T14:28:40.391Z",
+            "desc": "Android WebView \u4e0e JavaScript \u4ea4\u4e92",
+            "publishedAt": "2016-03-16T11:24:01.505Z",
+            "type": "Android",
+            "url": "http://mthli.github.io/Android-WebView-JavaScript/",
+            "used": true,
+            "who": "mthli"
+        },
+        {
+            "_id": "56cc6d23421aa95caa707abf",
+            "createdAt": "2015-07-09T13:29:56.411Z",
+            "desc": "\u9996\u6b21\u542f\u52a8\u5e94\u7528\u663e\u793a\u7684\u64cd\u4f5c\u6307\u793a",
+            "publishedAt": "2015-07-10T03:53:11.155Z",
+            "type": "Android",
+            "url": "https://github.com/worker8/TourGuide",
+            "used": true,
+            "who": "mthli"
+        },
+        {
+            "_id": "581f5267421aa91376974643",
+            "createdAt": "2016-11-06T23:55:19.19Z",
+            "desc": "App \u5907\u4efd\u548c\u6062\u590d\uff0c\u505a\u7684\u5f88\u5b8c\u6574",
+            "images": [
+                "http://img.gank.io/15759d56-d91c-4693-b5cf-d8dac9d019d3"
+            ],
+            "publishedAt": "2016-11-07T11:47:36.373Z",
+            "source": "web",
+            "type": "Android",
+            "url": "https://github.com/samanyougarg/ABR-App-Backup-Restore",
+            "used": true,
+            "who": "Samanyou Garg"
+        },
+        {
+            "_id": "56cc6d26421aa95caa707f61",
+            "createdAt": "2015-12-04T05:47:40.952Z",
+            "desc": "FinestWebView-Android",
+            "publishedAt": "2015-12-09T04:24:10.399Z",
+            "type": "Android",
+            "url": "https://github.com/TheFinestArtist/FinestWebView-Android",
+            "used": true,
+            "who": "wuzheng"
+        },
+        {
+            "_id": "56cc6d28421aa95caa7080b8",
+            "createdAt": "2015-12-30T02:02:37.465Z",
+            "desc": "Android \u8f6f\u952e\u76d8\u548cemoji\u8868\u60c5\u5207\u6362\u65b9\u6848\uff0c\u548c\u5fae\u4fe1\u51e0\u4e4e\u4e00\u6837\u7684\u4f53\u9a8c",
+            "publishedAt": "2015-12-30T04:14:04.818Z",
+            "type": "Android",
+            "url": "http://www.jianshu.com/p/328784b363b6?hmsr=toutiao.io&utm_medium=toutiao.io&utm_source=toutiao.io",
+            "used": true,
+            "who": "Jason"
+        },
+        {
+            "_id": "56cc6d23421aa95caa707c55",
+            "createdAt": "2015-07-24T23:52:27.551Z",
+            "desc": "Dagger Example",
+            "publishedAt": "2015-07-31T04:02:57.550Z",
+            "type": "Android",
+            "url": "https://github.com/swankjesse/android-dagger-example",
+            "used": true,
+            "who": "andyiac"
+        }
+    ]
+};
